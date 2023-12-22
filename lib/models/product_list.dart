44a4c909c041a5d0e6423bad4c5f94a34816c35c
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-12-22 12:55:30 -0300

    Added PopUpMenu to filter favorite products.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,8 +4,25 @@ import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final _items = dummyProdcts;
+  bool _showFavoritesOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((element) => element.isFavorite).toList();
+    }
+
+    return [..._items];
+  }
+
+  showFavoriteOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  showFavoriteAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
+  }
 
   addProduct(Product product) {
     _items.add(product);
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_grid.dart';
 
+enum FilterOptions {
+  favorite,
+  all,
+}
+
 class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final pvProductList = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (ctx) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.favorite) {
+                pvProductList.showFavoriteOnly();
+              } else {
+                pvProductList.showFavoriteAll();
+              }
+            },
+          ),
+        ],
       ),
-      body: ProductGrid(),
+      body: const ProductGrid(),
     );
   }
 }
-
