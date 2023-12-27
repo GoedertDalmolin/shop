769b8def63b82fe232bf42dfde6cff4b548a5172
GoedertDalmolin <gbrunodalmolin@gmail.com>
2023-12-27 10:15:13 -0300

    Added provider of order_list.dart.

---
+++ lib/models/order_list.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/order.dart';
+
+class OrderList with ChangeNotifier {
+  List<Order> _items = [];
+
+  List<Order> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  addOrder(Cart cart) {
+    _items.insert(
+      0,
+      Order(
+        id: Random().nextDouble().toString(),
+        total: cart.totalAmount,
+        products: cart.items.values.toList(),
+        date: DateTime.now(),
+      ),
+    );
+
+    notifyListeners();
+  }
+}
