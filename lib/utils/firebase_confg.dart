f16d70aa010f29fe0965709fe2a540a4c6eda66b
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-01-03 19:32:59 -0300

    Updated firebase_confg.dart.

--- lib/utils/firebase_confg.dart
+++ lib/utils/firebase_confg.dart
@@ -1,4 +1,7 @@
 class FirebaseConfig {
   // Put your DataBase URL here
-  static String urlDatabase = 'YOUR-DATABASE-URL-HERE';
+  static String urlDatabase = 'YOUR-FIREBASE-DATABASE-URL';
+
+  static String productRoute = '/products';
+  static String orderRoute = '/orders';
 }
