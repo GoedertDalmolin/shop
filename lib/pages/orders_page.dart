087d2621e89f2894a39226f18a2689fcb0b3c36d
GoedertDalmolin <gbrunodalmolin@gmail.com>
2024-01-04 13:37:28 -0300

    Added future builder to load future on orders_page.dart.

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -12,37 +12,64 @@ class OrdersPage extends StatefulWidget {
 }
 
 class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    Provider.of<OrderList>(context, listen: false).loadOrders().then((value) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
+  // bool _isLoading = true;
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //
+  //   Provider.of<OrderList>(context, listen: false).loadOrders().then((value) {
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
+    // final OrderList orders = Provider.of(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meus Pedidos'),
-      ),
-      drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.itemsCount,
-              itemBuilder: (ctx, index) {
-                return OrderWidget(order: orders.items[index]);
-              }),
-    );
+        appBar: AppBar(
+          title: const Text('Meus Pedidos'),
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.error != null) {
+              return const Center(
+                child: Text('Erro'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (context, orders, _) {
+                  return ListView.builder(
+                      itemCount: orders.itemsCount,
+                      itemBuilder: (ctx, index) {
+                        return OrderWidget(order: orders.items[index]);
+                      });
+                },
+              );
+            }
+
+            return Container();
+          },
+        )
+
+        // _isLoading
+        //     ? const Center(
+        //         child: CircularProgressIndicator(),
+        //       )
+        //     : ListView.builder(
+        //         itemCount: orders.itemsCount,
+        //         itemBuilder: (ctx, index) {
+        //           return OrderWidget(order: orders.items[index]);
+        //         }),
+        );
   }
 }
